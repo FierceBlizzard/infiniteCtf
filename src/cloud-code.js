e3e7540dd94bfbd8f70e48a0908560deca8dcef3
Iclyn Taero <43586417+FierceBlizzard@users.noreply.github.com>
2023-11-27 14:40:00 -0500

    got the image once it's running, It runs the php website

--- src/cloud-code.js
+++ src/cloud-code.js
@@ -4,15 +4,16 @@
 const projectId = 'infinite-ctf-405100';
 const zone = 'europe-central2-b'
 const instanceName = 'test1'
-const machineType = 'n1-standard-1';
-const sourceImage = 'projects/debian-cloud/global/images/family/debian-10';
-const networkName = 'global/networks/default';
 
 const compute = require('@google-cloud/compute');
 
+
 // Create a new instance with the values provided above in the specified project and zone.
-async function createInstance() {
+async function createInstance(projectId, zone, instanceName) {
   const instancesClient = new compute.InstancesClient();
+  const machineType = 'n1-standard-1';
+  const sourceImage = 'projects/debian-cloud/global/images/family/debian-10';
+  const networkName = 'global/networks/default';
 
   console.log(`Creating the ${instanceName} instance in ${zone}...`);
 
@@ -36,6 +37,7 @@ async function createInstance() {
         {
           // Use the network interface provided in the networkName argument.
           name: networkName,
+          addressType: "EXTERNAL"
         },
       ],
     },
@@ -54,7 +56,41 @@ async function createInstance() {
     });
   }
 
-  console.log('Instance created.');
+  console.log(response);
 }
 
-createInstance();
+async function deleteInstance(projectId, zone, instanceName) {
+  const instancesClient = new compute.InstancesClient();
+
+  console.log(`Deleting ${instanceName} from ${zone}...`);
+
+  const [response] = await instancesClient.delete({
+    project: projectId,
+    zone,
+    instance: instanceName,
+  });
+  let operation = response.latestResponse;
+  const operationsClient = new compute.ZoneOperationsClient();
+
+  // Wait for the delete operation to complete.
+  while (operation.status !== 'DONE') {
+    [operation] = await operationsClient.wait({
+      operation: operation.name,
+      project: projectId,
+      zone: operation.zone.split('/').pop(),
+    });
+  }
+
+  console.log('Instance deleted.');
+}
+async function getIP(projectId, zone, instanceName){
+  const compute = new Compute({projectId});
+  const [instance] = await compute({projectId}).zone(zone).instance(instanceName).get();
+  const networkInterfaces = instance.metadata.networkInterfaces;
+  const ipAddress = networkInterfaces[0].networkIP;
+  console.log(`IP: ${ipAddress}`);
+}
+
+createInstance(projectId, zone, instanceName);
+//getIP(projectId, zone, instanceName);
+//deleteInstance(projectId, zone, instanceName);
